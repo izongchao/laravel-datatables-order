f645b107f6a07496a55c6fa47bac0706d082f8ee
徐宗超 <xuzongchao@xuzongchaodeMacBook-Pro.local>
2019-05-20 14:20:29 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+<h1 align="center"> laravel-datatables-order </h1>
+
+<p align="center"> Datatables order by laravel..</p>
+
+
+## Installing
+
+```shell
+$ composer require izongchao/laravel-datatables-order -vvv
+```
+
+## Usage
+
+先在模型中use DatatablesOrder
+```php
+/**
+ * Class User
+ */
+class User extends Model
+{
+    use DatatablesOrder;
+}
+```
+调用排序
+```php
+
+User::datatablesOrder($request)->get();
+```
+ 
+
+
+## Contributing
+
+You can contribute in one of three ways:
+
+1. File bug reports using the [issue tracker](https://github.com/izongchao/laravel-datatables-order/issues).
+2. Answer questions or fix bugs on the [issue tracker](https://github.com/izongchao/laravel-datatables-order/issues).
+3. Contribute new features or update the wiki.
+
+_The code contribution process is not very formal. You just need to make sure that you follow the PSR-0, PSR-1, and PSR-2 coding guidelines. Any new code contributions must be accompanied by unit tests where applicable._
+
+## License
+
+MIT
---
+++ src/DatatablesOrder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Izongchao\LaravelDatatablesOrder;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Trait DatatablesOrder
+ *
+ * @package App\Traits
+ */
+trait DatatablesOrder
+{
+    /**
+     * 高级排序
+     *
+     * @param $query
+     * @param $request
+     *
+     * @return mixed
+     */
+    public function scopeDatatablesOrder($query, Request $request)
+    {
+        $key = array_get($request->all(), 'order.0.column', false);
+        $dir = array_get($request->all(), 'order.0.dir', false);
+
+        if ($key && $dir) {
+            $columnKey = 'columns.' . $key . '.data';
+            $column = array_get($request->all(), $columnKey, false);
+
+            if ($column && Schema::hasColumn($this->getTable(), $column)) {
+                if ($dir === 'desc') {
+                    return $query->latest($column);
+                }
+                return $query->oldest($column);
+            }
+        }
+
+        return $query;
+    }
+}
