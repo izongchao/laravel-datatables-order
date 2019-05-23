ab0f5abd5d4fba79e12345550f66c8a70ea01301
zongchao <zchao0723@126.com>
2019-05-23 03:58:51 +0000

    Apply fixes from StyleCI

--- src/DatatablesOrder.php
+++ src/DatatablesOrder.php
@@ -1,19 +1,26 @@
 <?php
 
+/*
+ * This file is part of the izongchao/gweather.
+ *
+ * (c) xuzongchao <zchao0723@126.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Izongchao\LaravelDatatablesOrder;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Schema;
 
 /**
- * Trait DatatablesOrder
- *
- * @package Izongchao\LaravelDatatablesOrder
+ * Trait DatatablesOrder.
  */
 trait DatatablesOrder
 {
     /**
-     * 高级排序
+     * 高级排序.
      *
      * @param $query
      * @param Request $request
@@ -26,13 +33,14 @@ trait DatatablesOrder
         $dir = array_get($request->all(), 'order.0.dir', false);
 
         if ($key && $dir) {
-            $columnKey = 'columns.' . $key . '.data';
+            $columnKey = 'columns.'.$key.'.data';
             $column = array_get($request->all(), $columnKey, false);
 
             if ($column && Schema::hasColumn($this->getTable(), $column)) {
-                if ($dir === 'desc') {
+                if ('desc' === $dir) {
                     return $query->latest($column);
                 }
+
                 return $query->oldest($column);
             }
         }
